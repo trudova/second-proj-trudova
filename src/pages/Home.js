70cce8f845e6aaa9f6e50faa4038f3edc3b3988d
trudova <liubovtrudova@gmail.com>
2020-08-18 10:09:00 -0400

    /implement servises to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
+import Services from '../components/Services'
 export default function Home() {
     return (
+        <>
         <Hero>
         <Banner title='luxurious rooms' subtitle='deluxe rooms just for $199'>
         <Link to='/rooms' className='btn-primary'>
@@ -11,5 +13,8 @@ export default function Home() {
         </Link>
         </Banner>
         </Hero >
+        <Services/>
+        </>
+
     )
 }
