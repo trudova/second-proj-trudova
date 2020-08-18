f73e6b28d3b2919cbd030613c67877b1b954f4ae
trudova <liubovtrudova@gmail.com>
2020-08-18 11:14:21 -0400

    create roomProvider component

---
+++ src/context.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+const RoomContext =React.createContext();
+export default class RoomProvider extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
