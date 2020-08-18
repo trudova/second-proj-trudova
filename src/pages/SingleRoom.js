a9282a295c87dff34c37731d397f941cd60beb26
trudova <liubovtrudova@gmail.com>
2020-08-18 19:08:49 -0400

    set up images for room

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -31,13 +31,22 @@ this.state={
         }
         const {name, description, capacity,size,price,extras,breakfast,pets,images } =room
         return (
-            <StyledHero img={images[0] || this.state.defaultBcg}>
+            <>
+            <StyledHero img={images[0] || this.state.defaultBcg }>
                 <Banner title={`${name} room`}>
                     <Link to='/rooms' className='btn-primary'>
                         back to rooms
                     </Link>
                 </Banner>
             </StyledHero>
+            <section className='single-room'>
+            <div className='single-room-images'>
+                {images.map((item, index)=>{
+                   return <img key={index} src={item} alt={name}/>
+                })}
+            </div>
+            </section >
+            </>
         )
     }
 }
