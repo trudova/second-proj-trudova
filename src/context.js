33ca2e604e6b0ce5bbbc2b18cc51ccfccba785c1
trudova <liubovtrudova@gmail.com>
2020-08-18 12:22:28 -0400

    add the methods to access data

--- src/context.js
+++ src/context.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import items from './data'
 const RoomContext =React.createContext();
 //provider and concumer 
 class RoomProvider extends Component {
     state={
-
+        rooms:[],
+        sortedRooms:[],
+        featuredRooms:[],
+        loading: true
     }
+    // getData 
+
+
+    componentDidMount(){
+ let rooms =this.formatData(items)
+    }
+formatData(items){
+    let  tempItems =items.map(item =>{
+        let id = item.sys.id
+        let images =item.fields.images.map(image=>{
+            image.fields.file.url
+        });
+    })
+}
+
     render() {
         return (
             <div>
