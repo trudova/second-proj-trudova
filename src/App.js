19c908fffdabf79b23103187cc3227aca45c5764
trudova <liubovtrudova@gmail.com>
2020-08-17 21:07:35 -0400

    set up Route component from react router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
+import {Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <>
-    <Home/>
-    <Rooms/>
-    <SingleRoom/>
-    <Error/>
+    <Route path='/' component={Home}/> 
+    <Route path='/rooms/' component={Rooms}/> 
+    <Route path='/single-room' component={SingleRoom}/> 
+    //<Route  component={Error}/> 
     </>
   );
 }
