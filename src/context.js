c931ce12001297c84ba5865340d56e5714652337
trudova <liubovtrudova@gmail.com>
2020-08-18 11:29:30 -0400

    FeaturedRooms added to the home

--- src/context.js
+++ src/context.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 const RoomContext =React.createContext();
-export default class RoomProvider extends Component {
+//provider and concumer 
+class RoomProvider extends Component {
+    state={
+
+    }
     render() {
         return (
             <div>
-                
+                <RoomContext.Provider value='hello'>
+                    {this.props.children}
+                </RoomContext.Provider>
             </div>
         )
     }
 }
+const RoomConsumer =RoomContext.Consumer;
+export {RoomProvider,RoomConsumer,RoomContext};
