ef67556a06aeafe2815f449623e27de2f940aad9
trudova <liubovtrudova@gmail.com>
2020-08-18 16:47:24 -0400

    set up the state

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -5,6 +5,15 @@ import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
 import {RoomContext} from '../context'
 export default class SingleRoom extends Component {
+    constructor(props){
+        super(props)
+this.state={
+    slug: this.props.match.params.slug
+}
+    }
+    // componentDidMount(){
+
+    // }
     render() {
         return (
             <div>
