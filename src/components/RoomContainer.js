024a0a0801c98f86ce62923b5d08d017aeb608cf
trudova <liubovtrudova@gmail.com>
2020-08-18 20:39:52 -0400

    room cont rebuilt with higth order component fron  context

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,27 +1,49 @@
-import React from 'react'
-import RoomFilter from './RoomFilter'
-import RoomList from './RoomList'
-import {RoomConsumer} from '../context'
-import Loading from './Loading'
-export default function RoomContainer() {
-    return (
-        <RoomConsumer>
-            {
-              (value)=>{
-                  const {loading, sortedRooms,rooms}=value;
-                  if(loading){
-                      return<Loading/>
-                  }
-                return(
-                    <div>
-            hello from rooms conteiner
-            <RoomFilter rooms={rooms}/>
-            <RoomList rooms={sortedRooms}/>
-        </div>
-                )
-              }  
-            }
-        </RoomConsumer>
-        
-    )
+import React from "react";
+import RoomsFilter from "./RoomFilter";
+import RoomsList from "./RoomList";
+import { withRoomConsumer } from "../context";
+import Loading from "./Loading";
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
+    </>
+  );
 }
+
+export default withRoomConsumer(RoomContainer);
+//rebuilt with higth order component fron  context
+
+// import React from 'react'
+// import RoomFilter from './RoomFilter'
+// import RoomList from './RoomList'
+// import {RoomConsumer} from '../context'
+// import Loading from './Loading'
+// export default function RoomContainer() {
+//     return (
+//         <RoomConsumer>
+//             {
+//               (value)=>{
+//                   const {loading, sortedRooms,rooms}=value;
+//                   if(loading){
+//                       return<Loading/>
+//                   }
+//                 return(
+//                     <div>
+//             hello from rooms conteiner
+//             <RoomFilter rooms={rooms}/>
+//             <RoomList rooms={sortedRooms}/>
+//         </div>
+//                 )
+//               }  
+//             }
+//         </RoomConsumer>
+        
+//     )
+// }
