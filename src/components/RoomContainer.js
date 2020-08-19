ec76082106d4f5e489c5a1805b4dc2beca4013a4
trudova <liubovtrudova@gmail.com>
2020-08-18 20:16:46 -0400

    RoomConsumer implement

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import RoomFilter from './RoomFilter'
 import RoomList from './RoomList'
+import {RoomConsumer} from '../context'
+import Loading from './Loading'
 export default function RoomContainer() {
     return (
-        <div>
+        <RoomConsumer>
+            {
+              (value)=>{
+                  const {loading, sortedRooms,rooms}=value;
+                return(
+                    <div>
             hello from rooms conteiner
             <RoomFilter/>
             <RoomList/>
         </div>
+                )
+              }  
+            }
+        </RoomConsumer>
+        
     )
 }
