8c6bb4266c9b6ffda358947c4b424404ae8d409b
trudova <liubovtrudova@gmail.com>
2020-08-18 21:48:31 -0400

    get contexr API set this up to  var

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -3,6 +3,8 @@ import {useContext} from 'react'
 import {RoomContext} from '../context'
 import Title from './Title'
 export default function RoomFilter() {
+    //get contexr API set this up to  var 
+    const context = useContext(RoomContext);
     return (
         <div>
             hello from filtr
